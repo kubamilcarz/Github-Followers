8a908397d81e99f3e98c63bc6523580f74debc7b
Kuba Milcarz <kubamilcarz@yahoo.com>
2024-01-12 14:19:19 +0100

    Textfield & Button push the search view

---
+++ Github Followers/FollowerListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FollowersListViewController.swift
+//  Github Followers
+//
+//  Created by Kuba Milcarz on 1/12/24.
+//
+
+import UIKit
+
+class FollowersListViewController: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
