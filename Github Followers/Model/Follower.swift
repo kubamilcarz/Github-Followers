c844b0d7c5abda120013853ce36944a77c8fcef8
Kuba Milcarz <kubamilcarz@yahoo.com>
2024-01-12 20:48:17 +0100

    fixed bug where navigation hidden wouldn't hide + created Follower & User models

---
+++ Github Followers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  Github Followers
+//
+//  Created by Kuba Milcarz on 1/12/24.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
