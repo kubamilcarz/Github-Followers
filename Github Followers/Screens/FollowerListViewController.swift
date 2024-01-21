ec7b68704ead128177b42d4de0a07f2b9746010f
Kuba Milcarz <kubamilcarz@yahoo.com>
2024-01-21 17:21:59 +0100

    Added Network Manager and fetched followers array

--- Github Followers/Screens/FollowerListViewController.swift
+++ Github Followers/Screens/FollowerListViewController.swift
@@ -16,6 +16,15 @@ class FollowerListViewController: UIViewController {
 
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
+            guard let followers else {
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage!, buttonTitle: "OK")
+                return
+            }
+            
+            print("Followers count = \(followers.count)")
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
