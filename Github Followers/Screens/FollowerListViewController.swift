e14fc78b1828d31a301b0c1924081c343115f3c9
Kuba Milcarz <kubamilcarz@yahoo.com>
2024-02-03 13:14:38 +0100

    Implemented Result type for error handling

--- Github Followers/Screens/FollowerListViewController.swift
+++ Github Followers/Screens/FollowerListViewController.swift
@@ -17,13 +17,14 @@ class FollowerListViewController: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
-            guard let followers else {
-                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage!.rawValue, buttonTitle: "OK")
-                return
+        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+            switch result {
+            case .success(let followers):
+                print(followers)
+                
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "OK")
             }
-            
-            print("Followers count = \(followers.count)")
         }
     }
     
