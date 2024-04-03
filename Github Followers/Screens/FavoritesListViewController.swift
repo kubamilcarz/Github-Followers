0f91221057412f408aa2925b6bb49fef70d64019
Kuba Milcarz <kubamilcarz@yahoo.com>
2024-04-03 15:14:48 +0200

    UserDefaults persistence added

--- Github Followers/Screens/FavoritesListViewController.swift
+++ Github Followers/Screens/FavoritesListViewController.swift
@@ -11,7 +11,14 @@ class FavoritesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemBlue
+        
+        PersistenceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                break
+            case .failure(let error):
+                break
+            }
+        }
     }
 }
--- Github Followers/Utilities/ErrorMessage.swift
+++ Github Followers/Utilities/ErrorMessage.swift
@@ -12,4 +12,6 @@ enum GFError: String, Error {
     case unableToComplete   = "Unable to complete your request. Please check your internet connection."
     case invalidResponse    = "Invalid response from the server. Please try again."
     case invalidData        = "Received data is invalid. Please try again."
+    case unableToFavorite   = "There was an error favoriting a user. Please try again."
+    case alreadyInFavorites = "You have already favorited this user. You must really like them!"
 }
